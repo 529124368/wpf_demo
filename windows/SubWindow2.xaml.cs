2571f5b9df792073450cd8350f99fe2292a46dde
529124368 <529124368@qq.com>
2023-01-19 15:40:35 +0900

    update

--- windows/SubWindow2.xaml.cs
+++ windows/SubWindow2.xaml.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using System.Windows;
-using System.Windows.Controls;
 using WpfApp1.models;
-using WpfApp1;
-using Autofac;
 
 namespace WpfApp1.windows
 {
@@ -23,7 +19,6 @@ namespace WpfApp1.windows
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
             _my.Course = new List<string> { "数学", "语文", "体育", "英语" };
             selectBox.DataContext = _my;
 
@@ -32,7 +27,7 @@ namespace WpfApp1.windows
             Menus a2 = new Menus("菜单2", null);
             Menus a4 = new Menus("菜单3", null);
             Menus a1_1 = new Menus("菜单1_1", a1);
-            Menus a1_2 = new Menus("菜单1_2", a1);
+            Menus a1_2 = new Menus("打开文件", a1);
             Menus a2_1 = new Menus("菜单2_1", a2);
             Menus a1_1_1 = new Menus("菜单1_1_1", a1_1);
             Menus a1_1_2 = new Menus("菜单1_1_2", a1_1);
@@ -46,6 +41,15 @@ namespace WpfApp1.windows
             box.Add(a4);
 
             menus.DataContext = box;
+
+            //游戏一览渲染
+            int[] ll = new int[9] { 1,2,3,4,5,6,7,8,9};
+            gameList.DataContext = ll;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"账号:{account.Text},密码:{password.Password}");
         }
     }
 
